8b4b4c8863099a1cf27396cec23e15eb29ea0fc5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-03-15 11:42:21 +1300

    Add support for block with buffer.

---
+++ test/logger/test_buffer.rb
@@ -0,0 +1,24 @@
+# coding: US-ASCII
+# frozen_string_literal: false
+require 'test/unit'
+require 'logger'
+
+class TestBuffer < Test::Unit::TestCase
+  def test_block_argument
+    r, w = IO.pipe
+    logger = Logger.new(w)
+    
+    logger.info do |buffer|
+      buffer.puts "Hello"
+      buffer.puts "World"
+    end
+    
+    IO.select([r], nil, nil, 0.1)
+    w.close
+    msg = r.read
+    r.close
+    
+    assert_include msg, "Hello"
+    assert_include msg, "World"
+  end
+end
