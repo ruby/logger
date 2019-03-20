ebd8dc14141515ccb3e434eb8c774981777e8757
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-03-21 12:29:22 +1300

    Default `logdev` to `$stderr`.

--- lib/logger.rb
+++ lib/logger.rb
@@ -372,7 +372,7 @@ class Logger
   #
   # Create an instance.
   #
-  def initialize(logdev, shift_age = 0, shift_size = 1048576, level: DEBUG,
+  def initialize(logdev = $stderr, shift_age = 0, shift_size = 1048576, level: DEBUG,
                  progname: nil, formatter: nil, datetime_format: nil,
                  shift_period_suffix: '%Y%m%d')
     self.level = level
