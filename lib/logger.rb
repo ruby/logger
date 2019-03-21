225f64479f76734356eda5c5126ecd8bb3208360
sonots <sonots@gmail.com>
2019-03-22 01:22:01 +0900

    Revert frozen_string_literal: true

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # logger.rb - simple logging utility
 # Copyright (C) 2000-2003, 2005, 2008, 2011  NAKAMURA, Hiroshi <nahi@ruby-lang.org>.
 #
--- lib/logger/version.rb
+++ lib/logger/version.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Logger
-  VERSION = "1.3.0"
+  VERSION = "1.3.0".freeze
 end
