d3dccbe971fe407e4775412beeba86e5c8c3664b
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-02-24 19:41:13 +0900

    Revert "Simplify the next/previous rotate calculations"
    This reverts commit 8a78af3705a2235e6123e053e66ac9f3bf124ea2.

--- lib/logger/period.rb
+++ lib/logger/period.rb
@@ -9,20 +9,21 @@ class Logger
     def next_rotate_time(now, shift_age)
       case shift_age
       when 'daily', :daily
-        delta = 1
+        t = Time.mktime(now.year, now.month, now.mday) + SiD
       when 'weekly', :weekly
-        delta = 7 - now.wday
+        t = Time.mktime(now.year, now.month, now.mday) + SiD * (7 - now.wday)
       when 'monthly', :monthly
-        delta = 32 - now.mday
-        mday = 1
+        t = Time.mktime(now.year, now.month, 1) + SiD * 32
+        return Time.mktime(t.year, t.month, 1)
       when 'now', 'everytime', :now, :everytime
         return now
       else
         raise ArgumentError, "invalid :shift_age #{shift_age.inspect}, should be daily, weekly, monthly, or everytime"
       end
-      t = now + delta * SiD
-      if t.hour.nonzero? or t.min.nonzero? or t.sec.nonzero? or (mday && t.mday != mday)
-        t = Time.mktime(t.year, t.month, mday || t.mday)
+      if t.hour.nonzero? or t.min.nonzero? or t.sec.nonzero?
+        hour = t.hour
+        t = Time.mktime(t.year, t.month, t.mday)
+        t += SiD if hour > 12
       end
       t
     end
@@ -30,18 +31,17 @@ class Logger
     def previous_period_end(now, shift_age)
       case shift_age
       when 'daily', :daily
-        delta = 0
+        t = Time.mktime(now.year, now.month, now.mday) - SiD / 2
       when 'weekly', :weekly
-        delta = now.wday
+        t = Time.mktime(now.year, now.month, now.mday) - (SiD * now.wday + SiD / 2)
       when 'monthly', :monthly
-        delta = now.mday - 1
+        t = Time.mktime(now.year, now.month, 1) - SiD / 2
       when 'now', 'everytime', :now, :everytime
         return now
       else
         raise ArgumentError, "invalid :shift_age #{shift_age.inspect}, should be daily, weekly, monthly, or everytime"
       end
-      t = now - delta * SiD
-      Time.mktime(t.year, t.month, t.mday) - 1
+      Time.mktime(t.year, t.month, t.mday, 23, 59, 59)
     end
   end
 end
