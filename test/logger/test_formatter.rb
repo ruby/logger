ab4dc5ad5e58eac42e5ca7008e5acab49e17126f
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-08-13 15:46:22 +0100

    add support in default formatter to receive extra context as kwargs

--- test/logger/test_formatter.rb
+++ test/logger/test_formatter.rb
@@ -9,12 +9,39 @@ class TestFormatter < Test::Unit::TestCase
     progname = 'ruby'
     msg = 'This is a test'
     formatter = Logger::Formatter.new
+    time_matcher = /\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}/
 
     result = formatter.call(severity, time, progname, msg)
-    time_matcher = /\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}/
     matcher = /#{severity[0..0]}, \[#{time_matcher} #\d+\]  #{severity} -- #{progname}: #{msg}\n/
 
     assert_match(matcher, result)
+  end
+
+  def test_call_with_context
+    severity = 'INFO'
+    time = Time.now
+    progname = 'ruby'
+    msg = 'This is a test'
+    formatter = Logger::Formatter.new
+    time_matcher = /\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}/
+
+    # context as hash
+    result = formatter.call(severity, time, progname, msg, context: { foo: "bar" })
+    matcher = /#{severity[0..0]}, \[foo=bar\] \[#{time_matcher} #\d+\]  #{severity} -- #{progname}: #{msg}\n/
+    assert_match(matcher, result)
+
+    # context as array
+    result = formatter.call(severity, time, progname, msg, context: ["tag"])
+    matcher = /#{severity[0..0]}, \[tag\] \[#{time_matcher} #\d+\]  #{severity} -- #{progname}: #{msg}\n/
+    assert_match(matcher, result)
+
+    # context as nil
+    result = formatter.call(severity, time, progname, msg, context: nil)
+    matcher = /#{severity[0..0]}, \[#{time_matcher} #\d+\]  #{severity} -- #{progname}: #{msg}\n/
+    assert_match(matcher, result)
+
+    # unsupported context
+    assert_raise(Logger::Error) { formatter.call(severity, time, progname, msg, context: Object.new) }
   end
 
   class CustomFormatter < Logger::Formatter
